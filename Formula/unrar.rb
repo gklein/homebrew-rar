00ced0fb83952c749f53c3c1c42e618b82dfd3ad
Gil Klein <gklein@gmail.com>
2026-03-06 19:59:40 +0200

    Add unrar formula v7.2.4

---
+++ Formula/unrar.rb
@@ -0,0 +1,52 @@
+class Unrar < Formula
+  desc "Extract, view, and test RAR archives"
+  homepage "https://www.rarlab.com/"
+  url "https://www.rarlab.com/rar/unrarsrc-7.2.4.tar.gz"
+  sha256 "b02e571a33af7711cd803080500370dc1d28eea82b2032480819d27462ad8b31"
+  license :cannot_represent
+
+  livecheck do
+    url "https://www.rarlab.com/rar_add.htm"
+    regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  def install
+    # upstream doesn't particularly care about their unix targets,
+    # so we do the dirty work of renaming their shared objects to
+    # dylibs for them.
+    inreplace "makefile", "libunrar.so", "libunrar.dylib" if OS.mac?
+
+    system "make"
+    bin.install "unrar"
+
+    # Explicitly clean up for the library build to avoid an issue with an
+    # apparent implicit clean which confuses the dependencies.
+    system "make", "clean"
+    system "make", "lib"
+    lib.install shared_library("libunrar")
+
+    prefix.install "license.txt"
+  end
+
+  def caveats
+    <<~EOS
+      We agreed to the UnRAR license for you:
+        #{opt_prefix}/license.txt
+      If this is unacceptable you should uninstall the formula.
+    EOS
+  end
+
+  test do
+    contentpath = "directory/file.txt"
+    rarpath = testpath/"archive.rar"
+    data =  "UmFyIRoHAM+QcwAADQAAAAAAAACaCHQggDIACQAAAAkAAAADtPej1LZwZE" \
+            "QUMBIApIEAAGRpcmVjdG9yeVxmaWxlLnR4dEhvbWVicmV3CsQ9ewBABwA="
+
+    rarpath.write data.unpack1("m")
+    assert_equal contentpath, shell_output("#{bin}/unrar lb #{rarpath}").strip
+    assert_equal 0, $CHILD_STATUS.exitstatus
+
+    system "#{bin}/unrar", "x", rarpath, testpath
+    assert_equal "Homebrew", (testpath/contentpath).read.chomp
+  end
+end
